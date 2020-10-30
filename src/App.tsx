95e4ac786552dfb8d4128988e87fb0228cf2656a
BOBOWang <bbw1996@qq.com>
2020-10-31 00:32:56 +0800

    创建 Layout 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,65 @@
 import React from 'react';
-import styled from 'styled-components';
 import 'index.scss';
-import Nav from './components/Nav';
-
 import {
   HashRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
+import Layout from 'components/Layout';
 
-const Wrapper = styled.div`
-  min-height:100vh;
-  display:flex;
-  flex-direction:column;
-`;
-const Main = styled.div`
-  flex-grow:1;
-  overflow:auto;
-`;
+
 
 
 function App() {
   return (
     <Router>
-      <Wrapper>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags />
-            </Route>
-            <Route path="/money">
-              <Money />
-            </Route>
-            <Route path="/statistics">
-              <Statistics />
-            </Route>
-            <Redirect exact from="/" to="/money" />
-            <Route path="*">
-              <NoMatch />
-            </Route>
-          </Switch>
-        </Main>
-        <Nav />
-      </Wrapper>
+      <Switch>
+        <Route path="/tags">
+          <Tags />
+        </Route>
+        <Route path="/money">
+          <Money />
+        </Route>
+        <Route path="/statistics">
+          <Statistics />
+        </Route>
+        <Redirect exact from="/" to="/money" />
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
     </Router>
   );
 }
 function NoMatch() {
   return (
-    <div>页面不存在，你输错地址了</div>
+    <Layout>
+      <div>页面不存在，你输错地址了</div>
+    </Layout>
   );
 }
 function Statistics() {
-  return <h2>统计页面</h2>;
+  return (
+    <Layout>
+      <h2>统计页面</h2>
+    </Layout>
+  );
 }
 
 function Tags() {
-  return <h2>标签页面</h2>;
+  return (
+    <Layout>
+      <h2>标签页面</h2>
+    </Layout>
+  );
 }
 
 function Money() {
-  return <h2>记账页面</h2>;
+  return (
+    <Layout>
+      <h2>记账页面</h2>
+    </Layout>
+  );
 }
 export default App;
