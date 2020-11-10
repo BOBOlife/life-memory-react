2b62d502b15e3f950d342388c1968c6ecb465404
BOBOWang <bbw1996@qq.com>
2020-11-10 21:21:51 +0800

    Money.tsx 之类型 UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -41,7 +41,27 @@ const NotesSection = styled.section`
   }
 `;
 const CategorySection = styled.section`
-
+  font-size: 24px;
+  > ul {
+    display: flex;
+    background: #c4c4c4;
+    > li {
+      width: 50%;
+      text-align: center;
+      padding:15px 0;
+      position: relative;
+      &.selected::after {
+        content: '';
+        display: block;
+        height: 2px;
+        background: #333;
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        left: 0;
+      }
+    }
+  }
 `;
 const NumberPadSection = styled.section`
 
@@ -68,7 +88,7 @@ function Money() {
       </NotesSection>
       <CategorySection>
         <ul>
-          <li>支出</li>
+          <li className="selected">支出</li>
           <li>收入</li>
         </ul>
       </CategorySection>
