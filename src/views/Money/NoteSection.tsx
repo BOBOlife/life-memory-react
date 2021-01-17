631c2e8e47fb111904ef9b7020073084fce74e40
wangbo <bbw1996@qq.com>
2021-01-18 01:04:14 +0800

    收集表单数据

--- src/views/Money/NoteSection.tsx
+++ src/views/Money/NoteSection.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import * as React from 'react';
-import {useRef, useState} from 'react';
+import {useRef} from 'react';
 
 const Wrapper = styled.section`
   background: #f5f5f5;
@@ -26,12 +26,17 @@ const Wrapper = styled.section`
   }
 `;
 
-const NoteSection: React.FC = () => {
-  const [note, setNote] = useState('');
+type Props = {
+  value: string,
+  onChange: (value: string) => void
+}
+
+const NoteSection: React.FC<Props> = (props) => {
+  const note = props.value;
   const refInput = useRef<HTMLInputElement>(null);
   const onBlur = () => {
     if (refInput.current !== null) {
-      setNote(refInput.current.value);
+      props.onChange(refInput.current.value);
     }
   };
   return (
