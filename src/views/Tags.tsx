5ad882bdc7291550e166c67c50dc38403f8ea7da
wangbo <bbw1996@qq.com>
2021-01-18 23:17:24 +0800

    抽离 useTags 函数

--- src/views/Tags.tsx
+++ src/views/Tags.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import Layout from 'components/Layout';
+import {useTags} from 'useTags';
+
 function Tags() {
-    return (
-        <Layout>
-            <h2>标签页面</h2>
-        </Layout>
-    );
+  const {tags, setTags} = useTags();
+  return (
+    <Layout>
+      <ol>
+        {tags.map(tag =>
+          <li key={tag}>{tag}</li>
+        )}
+      </ol>
+    </Layout>
+  );
 }
 
 export default Tags;
