c81f30edb641855f85a71ca6d91b860b7185ed3b
BOBOlife <36833642+BOBOlife@users.noreply.github.com>
2021-01-21 18:07:20 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ cd build &&
 git init &&
 git add .
 git commit -m "deploy" &&
-git remote add origin git@github.com:BOBOlife/packetlife-react_website.git &&
-git branch -M Main
+git remote add origin git@github.com:BOBOlife/packetlife-react_webmsite.git &&
+git branch -M main
 git push -u origin Main -f
 cd -
