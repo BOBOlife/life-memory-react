83bb092962e8a9c82b6d8f509c9942d48572aab9
wangbo <bbw1996@qq.com>
2021-01-22 02:11:20 +0800

    添加自动部署脚本文件

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ cd build &&
 git init &&
 git add .
 git commit -m "deploy" &&
-git remote add origin git@github.com:BOBOlife/packetlife-react_webmsite.git &&
+git remote add origin git@github.com:BOBOlife/packetlife-react_website.git &&
 git branch -M main
 git push -u origin main -f
 cd -
